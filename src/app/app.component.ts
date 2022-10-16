36e3cfd85483aeb223b9fb18e8a42b4ab3ae6ed7
Patricia Saavedra <patr.saavedra@duocuc.cl>
2022-10-16 00:57:56 -0300

    cambio pagina inicio y restablecer contraseña, orden de pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/inicio', icon: 'home' },
     { title: 'Invitado', url: '/home/invitado', icon: 'home' },
-    { title: 'Ingreso', url: '/ingreso', icon: 'enter' },
     { title: 'Restablecer Contraseña', url: '/restablecer-contra', icon: 'build' },
     { title: 'Registro Asistencia', url: '/registro-asistencia', icon: 'book' },
+    { title: 'Cerrar Sesion', url: '/inicio', icon: 'enter' },
   ];
 
   constructor() {}
