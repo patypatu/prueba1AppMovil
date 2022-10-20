afb0635b1325b6a2cb48e56d528b64dad24777cf
FranCastilloA <fr.castilloa@duocuc.cl>
2022-10-20 01:47:53 -0300

    se agrega nombre en el login
    se cambia el orden en el viewlogin

--- ApiRestQR/arQR/viewslogin.py
+++ ApiRestQR/arQR/viewslogin.py
@@ -10,6 +10,43 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 from rest_framework.authtoken.models import Token
 
+
+@api_view(['POST'])
+def login(request):
+    data = JSONParser().parse(request)
+ 
+    username = data['username']
+    password = data['password']
+
+    mensajeToken = {
+        'mensaje' : '',
+        'token' : '',
+        'nombre' : ''
+    }
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        mensajeToken['mensaje'] = "Usuario incorrecto"
+        return Response(mensajeToken,status=status.HTTP_404_NOT_FOUND)
+    
+    if password == user.password:
+        pass_valido = True
+    else:
+        pass_valido = False
+    
+    if not pass_valido:
+        mensajeToken['mensaje'] = "Contraseña incorrecta"
+        return Response(mensajeToken,status=status.HTTP_401_UNAUTHORIZED)
+ 
+    token, created = Token.objects.get_or_create(user=user)
+
+    #print(token.key)
+    mensajeToken['mensaje'] = "Clave correcta"
+    mensajeToken['token'] = token.key
+    mensajeToken['nombre'] = user.first_name
+    #mensajeToken['user'] = UserSerializer(user).data
+    return Response(mensajeToken,status=status.HTTP_200_OK)
  
 @api_view(['POST'])
 def login_profe(request):
@@ -43,37 +80,3 @@ def login_profe(request):
     return Response(mensajeToken,status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
-def login(request):
-    data = JSONParser().parse(request)
- 
-    username = data['username']
-    password = data['password']
-
-    mensajeToken = {
-        'mensaje' : '',
-        'token' : ''
-    }
-
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        mensajeToken['mensaje'] = "Usuario incorrecto"
-        return Response(mensajeToken,status=status.HTTP_404_NOT_FOUND)
-    
-    if password == user.password:
-        pass_valido = True
-    else:
-        pass_valido = False
-    
-    if not pass_valido:
-        mensajeToken['mensaje'] = "Contraseña incorrecta"
-        return Response(mensajeToken,status=status.HTTP_401_UNAUTHORIZED)
- 
-    token, created = Token.objects.get_or_create(user=user)
-
-    #print(token.key)
-    mensajeToken['mensaje'] = "Clave correcta"
-    mensajeToken['token'] = token.key
-    #mensajeToken['user'] = UserSerializer(user).data
-    return Response(mensajeToken,status=status.HTTP_200_OK)
